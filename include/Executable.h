83f2c0c38a107c0ec2e2d9559f390d8e8c431051
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-05 10:37:20 -0500

    feat(executable): add executable abstract type.

---
+++ include/Executable.h
@@ -0,0 +1,31 @@
+#ifndef EXECUTABLE_H
+#define EXECUTABLE_H
+
+#include "Resource.h"
+#include <string>
+#include <vector>
+
+class Executable {
+protected:
+    std::string name;
+    std::string description;
+    std::vector<std::string> requiredResourcesNames;
+    int durationInUnits;
+    std::vector<Resource*> assignedResources;
+public:
+    Executable(
+            const std::string& name,
+            const std::string& description,
+            const std::vector<std::string>& requiredResourcesNames,
+            int durationInUnits);
+    virtual ~Executable() = default;
+    [[nodiscard]] std::string getName() const;
+    [[nodiscard]] const std::vector<std::string>& getRequiredResourcesName() const;
+    [[nodiscard]] int getDurationInUnits() const;
+    void assignResources(const std::vector<std::unique_ptr<Resource>>& resourcePool);
+    void releaseResources();
+    virtual void execute() const = 0;
+    [[nodiscard]] bool canExecute(const std::vector<std::unique_ptr<Resource>>& resourcePool) const;
+};
+
+#endif //EXECUTABLE_H
