f71c91d2f0ae065a9af2f7196cd8985e8b54eb17
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-08 08:39:11 -0500

    feat(task): add task type implementation.

---
+++ src/Task.cpp
@@ -0,0 +1,18 @@
+#include "Task.h"
+#include <iostream>
+
+Task::Task(const std::string &name, const std::string &description,
+           const std::vector<std::string> &requiredResourcesNames, int durationInUnits)
+           : Executable(name, description, requiredResourcesNames, durationInUnits)
+           {}
+
+void Task::execute() const {
+    if (assignedResources.size() != requiredResourcesNames.size()) {
+        throw std::runtime_error("Not all required resources are assigned for task " + name + ".");
+    }
+    std::cout << "Executing task: " << name
+    << ": " << description << " (Duration: " << durationInUnits << " units)"  << std::endl;
+    for (const auto* resource : assignedResources) {
+        resource->use();
+    }
+}
